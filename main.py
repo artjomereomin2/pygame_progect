edb232ed74dbe1651880cdc101e72fa12080d55f
artjomereomin2 <artjomereomin2@yandex.ru>
2022-01-07 21:52:06 +0400

    start of inventory

--- main.py
+++ main.py
@@ -696,7 +696,9 @@ def planet_game(num):
                     _tile.save(num)
                 terminate()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
+                if event.key == pygame.K_i:
+                    pass
+                elif event.key == pygame.K_RIGHT:
                     player.move(tile_width, 0)
                     cost, count = show_action(player, num)
                 elif event.key == pygame.K_LEFT:
